524706d028805377c81f88e093f41d4edc0e2d10
ChipaKraekn <chipalino@live.com>
2015-07-16 15:18:07 +0600

    early alpha of iMarkets application
    used:
    	-framework7
    	-template7
    	-**magic**

---
+++ scripts/script.js
@@ -0,0 +1,22 @@
+﻿$("#home_btn").click(function () {
+    $("#block").html($('#home').html());
+});
+$("#wishlist_btn").click(function () {
+    $("#block").html($('#wishlist').html());
+});
+$("#categories_btn").click(function () {
+    $("#block").html($('#categories').html());
+});
+$("#map_btn").click(function () {
+    $("#block").html($('#map').html());
+});
+$("#add_btn").click(function () {
+    $("#block").html($('#add').html());
+});
+
+$("#block").html($('#home').html());
+
+$('.item').hover(function (e) {
+    $(this).addClass("black_active");
+    $(this).siblings().removeClass("black_active");
+})
